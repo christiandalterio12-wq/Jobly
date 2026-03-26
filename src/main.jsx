b276c248c57a84771d8d85b621ffbfa1c5f3f96b
christiandalterio12-wq <christiandalterio12@gmail.com>
2026-03-27 00:38:27 +0100

    Update main.jsx
    fix main file

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,10 @@
-git status
-git add .
-git commit -m "update jobly UI + CV studio"
-git push
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./styles.css";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
